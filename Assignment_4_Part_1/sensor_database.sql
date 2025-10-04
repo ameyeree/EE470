b570b719cc7069058bf5379f259e83508c95fc8a
ameyeree <161140957+ameyeree@users.noreply.github.com>
2025-10-04 10:19:54 -0700

    Update sensor_database.sql
    Initial sensor_data creation notes

--- Assignment_4_Part_1/sensor_database.sql
+++ Assignment_4_Part_1/sensor_database.sql
@@ -1,1 +1,32 @@
+SHOW DATABASES;
 
+-- Use desired database (at this time, my only database)
+USE u301268454_AnthonyMeyer;
+
+-- Create Two Tables sensor_data and sensor_register
+
+
+-- sensor_register Table
+/*
+node_name: Up to 10 Characters (KEY)
+manufacturer: Up to 10 characters
+longitude: a REAL positive/negative number with max of 15 positions
+latitude: a real (positive/negative) number wiht max of 15 positions
+NOTE: Since this is the sensor register, this is where the PRIMARY KEY is
+*/
+
+-- sensor_data Table
+/*
+node_name: Up to 10 Characters (KEY)
+time_received: Date and Time
+temperature: numeric value between -10 to +100 C maximum of 6 positions
+humiditiy: a number value between 0-100 with maximum of 6 positions
+-- Assumption: positions is number of digits. 
+*/
+CREATE TABLE sensor_data (
+	node_name VARCHAR(10) NOT NULL,
+    time_received DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    temperature DECIMAL(6,3) CHECK (NOT NULL AND temperature >= -10 AND temperature <= 100),
+    humidity DECIMAL(6,3) CHECK (NOT NULL AND humidity >= 0 AND humidity <= 100),
+    FOREIGN KEY (node_name) REFERENCES sensor_register(node_name)
+);
