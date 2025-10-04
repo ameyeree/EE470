5acebe2cad5e47e0d826ccc4fa01e7818a190721
ameyeree <161140957+ameyeree@users.noreply.github.com>
2025-10-04 07:41:48 -0700

    Update section_1_1_tutorial_commands.sql

--- Assignment_4_Part_1/section_1_1_tutorial_commands.sql
+++ Assignment_4_Part_1/section_1_1_tutorial_commands.sql
@@ -1,1 +1,22 @@
+SHOW DATABASES;
 
+-- Use appropriate Databasse
+USE u301268454_AnthonyMeyer;
+
+-- Create a table
+CREATE TABLE Book(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Insert First book into Table
+INSERT INTO Book (id, name) VALUES
+(1, 'MariaDB Book');
+
+-- Insert Second book into Table
+INSERT INTO Book (id, name) VALUES
+(2, 'SQLDB Book');
+
+-- Display all contents of Book Table
+SELECT * from Book;
